351aeed5858de8ad5d526e82a2b4bdfc53cba40d
Chetonix <chetonix@gmail.com>
2021-12-21 16:35:44 +0530

    editing recursive soln a little

--- Algos-DataStructs-Python-Treehouse/recursive_binary_search.py
+++ Algos-DataStructs-Python-Treehouse/recursive_binary_search.py
@@ -7,13 +7,15 @@ def recursive_binary_search(list, target):
             return True
         else: 
             if target > list[mid_point]:
+                # return recursive_binary_search(list[mid_point+1:], target)
                 return recursive_binary_search(list[mid_point+1:], target)
             else: 
+                # return recursive_binary_search(list[:mid_point], target)  
                 return recursive_binary_search(list[:mid_point], target)  
 
 def verify(result):
     print("Target found: ", result)
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-result = recursive_binary_search(numbers, 6)
-verify(result)
+result = recursive_binary_search(numbers, 2)
+verify(result) 
