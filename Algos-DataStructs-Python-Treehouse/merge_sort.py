9f1d6660885c37931dab3531d1419a11c5a06ad1
Chetonix <chetonix@gmail.com>
2021-12-28 16:57:10 +0530

    add merge_sort.py edited file

--- Algos-DataStructs-Python-Treehouse/merge_sort.py
+++ Algos-DataStructs-Python-Treehouse/merge_sort.py
@@ -1,18 +1,19 @@
- def merge_sort(list):
-      """
+def merge_sort(list):
+
+    """
       Sorts a list in ascending order
       Returns a new sorted list
       Uses Divide and Conquer methodology
-      """
+    """
 
     if len(list) <= 1 :
         return list
      
-     left_half, right_half = split(list)
-     left = merge_sort(left_half)
-     right = merge_sort(right_half)
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
 
-     return merge(left, right)
+    return merge(left, right)
 
 def split(list):
     """
@@ -43,4 +44,29 @@ def merge(left, right):
         else :
             l.append(right[j])
             j+=1
+
+    while i < len(left) :
+        l.append(left[i])
+        i+=1
+    while j < len(right):
+        l.append(right[j])
+        j+=1
+    
+    return l
+
+def verify_sorted(list):
+    n = len(list)
+
+    if n == 0 or n == 1:
+        return True
+
+    return list[0] <= list[1] and verify_sorted(list[1:])
+
+
+alist = [1, 4, 5, 75 , 56, 6, 7, 67, 4, 3, 45, 56, 67, 78 ,12, 2, 4, 5 , 46]
+l = merge_sort(alist) 
+# print(l)
+print(verify_sorted(alist)) 
+print(verify_sorted(l))
+ 
 
